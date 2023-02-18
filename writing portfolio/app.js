a46eadb997100b522f60137f4503299ec358d5e9
Lee <xianxianif@gmail.com>
2023-02-18 12:35:31 -0500

    uploading  folder from my system

---
+++ writing portfolio/app.js
@@ -0,0 +1,7 @@
+//Hamburger Toggle Button
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => { 
+    navbarLinks.classList.toggle('active')
+})
